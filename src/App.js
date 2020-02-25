b2dd5e76be0ea9db0b3102876f35b38c7b536c0e
Dimitrios Tikas <dimitrios.tikas@cpacsystems.se>
2020-02-25 21:23:14 +0100

    added packages and made requests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  useEffect(()=>{
+    axios.post("http://localhost:3000/api/v1/auth/signin",
+      {email: "admin", password: "admin"}
+    ).then(response => {
+
+      console.log(response.data.token)
+      axios.get("http://localhost:3000/api/v1/words",{
+        headers: {Authorization: `Bearer ${response.data.token}`}
+      }).then(response2 => {
+
+        console.log(response2.data);
+
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+    }).catch(error => {
+      console.log(error)
+    });
+  }, []);
+
+
   return (
     <div>
       
